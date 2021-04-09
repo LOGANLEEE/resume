d1dbd25992901073c35d0a2dd3c33199dcf80394
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-09 16:57:13 +0900

    imrpv Right

--- src/Containers/Right/Wrapper.ts
+++ src/Containers/Right/Wrapper.ts
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
 	flex-direction: column;
 	flex-wrap: wrap;
 	gap: 10px;
-
+	font-size: 1.6rem;
 	.block1 {
 		width: 100%;
 	}
--- src/Containers/Right/index.tsx
+++ src/Containers/Right/index.tsx
@@ -1,16 +1,23 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react';
 import { Wrapper } from './Wrapper';
 import { Carrier } from 'Components/Carrier';
+import { SkillList } from 'Components/SkillList';
+import { Activity } from 'Components/Activity';
+import { Divider } from '@material-ui/core';
 
 interface Props {}
 
-export const Right: FunctionComponent<Props & HTMLAttributes<HTMLDivElement>> = ({ className }) => (
-	<Wrapper className={className}>
-		<div className='block1'>
-			<Carrier />
-		</div>
-		<div>사용기술</div>
-		<div>활동내역</div>
-		<div>학력</div>
-	</Wrapper>
-);
+export const Right: FunctionComponent<Props & HTMLAttributes<HTMLDivElement>> = ({ className }) => {
+	return (
+		<Wrapper className={className}>
+			<div className='block1'>
+				<Carrier />
+			</div>
+			<Divider />
+			<SkillList />
+			<Divider />
+			<Activity />
+			<Divider />
+		</Wrapper>
+	);
+};

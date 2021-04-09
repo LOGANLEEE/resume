ad45eb4d5ee4102288302ab965e481bc61990200
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-09 16:56:20 +0900

    add Activity

---
+++ src/Components/Activity/Wrapper.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	font-size: 1.6rem;
+
+	.list {
+		padding: 1%;
+		.btn {
+			padding: 0;
+			margin: 0;
+			font-size: 1.6rem;
+		}
+	}
+	cursor: pointer;
+`;

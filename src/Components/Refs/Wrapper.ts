ff3b8629f7c68d81f33f877d1922e700bf0f4ef1
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-09 16:57:02 +0900

    add Refs

--- src/Components/Refs/Wrapper.ts
+++ src/Components/Refs/Wrapper.ts
@@ -2,11 +2,29 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	display: flex;
-	flex-direction: row;
+	flex-direction: column;
 	flex-wrap: wrap;
 	justify-content: space-evenly;
 
+	.block1 {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
+
+	.block2 {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
+
 	.icon {
 		cursor: pointer;
+
+		&:hover {
+			color: white;
+		}
 	}
 `;

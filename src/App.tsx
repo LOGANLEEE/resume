df3a16c627cb34735f800cdcc811577292a4d498
LOGANLEEE <logan_lee@kakao.com>
2021-12-24 21:24:55 +0000

    add .yarn
    add .gitignore
    
    apply detecting mobile and PC

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<BrowserView>
+				<h1>This is rendered only in browser</h1>
+			</BrowserView>
+			<MobileView>
+				<h1>This is rendered only on mobile</h1>
+			</MobileView>
+		</>
 	);
 }
 

74a2b1e6fe0e80b12134fdb2e36d80a0bb2bd335
LOGANLEEE <logan_lee@kakao.com>
2021-12-27 22:45:54 +0000

    apply animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { AppWrapper } from 'AppWrapper';
+import { ForMobile } from 'containers/ForMobile';
+import { ForPC } from 'containers/ForPC';
+import React, { useEffect, useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
+import { CSSTransition } from 'react-transition-group';
+
+/* todo 
+
+entering animation
+
+*/
 
 function App() {
+	const [reveal, setReveal] = useState(false);
+	useEffect(() => {
+		setTimeout(() => {
+			setReveal(true);
+		}, 500);
+	}, []);
 	return (
-		<>
-			<BrowserView>
-				<h1>This is rendered only in browser</h1>
-			</BrowserView>
-			<MobileView>
-				<h1>This is rendered only on mobile</h1>
-			</MobileView>
-		</>
+		<AppWrapper>
+			<CSSTransition
+				in={reveal}
+				timeout={200}
+				classNames='init'
+				unmountOnExit
+				onEnter={() => setReveal(true)}
+				onExited={() => setReveal(false)}>
+				<>
+					<BrowserView>
+						<ForPC />
+					</BrowserView>
+					<MobileView>
+						<ForMobile />
+					</MobileView>
+				</>
+			</CSSTransition>
+		</AppWrapper>
 	);
 }
 

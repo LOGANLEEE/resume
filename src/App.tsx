addfe1bab263eb030cd862d81b59243387075d65
LOGANLEEE <logan_lee@kakao.com>
2021-12-29 23:51:07 +0000

    remove animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { AppWrapper } from 'AppWrapper';
 import { ForMobile } from 'containers/ForMobile';
 import { ForPC } from 'containers/ForPC';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserView, MobileView } from 'react-device-detect';
-import { CSSTransition } from 'react-transition-group';
 
 /* todo 
 
@@ -12,30 +11,14 @@ entering animation
 */
 
 function App() {
-	const [reveal, setReveal] = useState(false);
-	useEffect(() => {
-		setTimeout(() => {
-			setReveal(true);
-		}, 500);
-	}, []);
 	return (
 		<AppWrapper>
-			<CSSTransition
-				in={reveal}
-				timeout={200}
-				classNames='init'
-				unmountOnExit
-				onEnter={() => setReveal(true)}
-				onExited={() => setReveal(false)}>
-				<>
-					<BrowserView>
-						<ForPC />
-					</BrowserView>
-					<MobileView>
-						<ForMobile />
-					</MobileView>
-				</>
-			</CSSTransition>
+			<BrowserView>
+				<ForPC />
+			</BrowserView>
+			<MobileView>
+				<ForMobile />
+			</MobileView>
 		</AppWrapper>
 	);
 }

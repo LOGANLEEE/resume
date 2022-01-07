c84519edf37fbbe5468b908934a5be1e452ad951
LOGANLEEE <logan_lee@kakao.com>
2022-01-07 22:29:57 +0000

    add timeline items

---
+++ src/containers/ForMobile/timeLineItems.ts
@@ -0,0 +1,90 @@
+import { TimelineItemModel } from 'react-chrono/dist/models/TimelineItemModel';
+
+export const companyItems: TimelineItemModel[] = [
+	{
+		title: '06.2018',
+		url: 'http://www.kb-sys.co.kr/en/index',
+		cardTitle: `KBSYS(SI)`,
+		cardSubtitle: '$9 million in annual sales',
+		cardDetailedText: [
+			`Developed open source based solutions.`,
+			`Developed cloud environment-oriented portal service and data visualization platform.`,
+		],
+	},
+	{
+		title: ' ',
+		url: 'https://eng.sk.com',
+		cardTitle: `SK C&C`,
+		cardSubtitle: '$1.8 billion in annual sales',
+		cardDetailedText: [`Developed "Digital Workplace" platform as full stack developer`],
+	},
+	{
+		title: ' ',
+		url: 'https://eng.sk.com',
+		cardTitle: `SK C&C`,
+		cardSubtitle: '$1.8 billion in annual sales',
+		cardDetailedText: [`Developed "ChatBot(IBM Watson)" platform as full stack developer`],
+	},
+	{
+		title: ' ',
+		url: 'https://www.magnachip.com',
+		cardTitle: 'MagnaChip (semiconductor manufacturing)',
+		cardSubtitle: '$567 million in annual sales',
+		cardDetailedText: [`Migrated Legacy System and Developed groupware as full stack developer`],
+	},
+	{
+		title: '06.2021',
+		url: 'https://en.wikipedia.org/wiki/Woori_Bank',
+		cardTitle: 'Woori Bank',
+		cardSubtitle: '$26 billin in annual sales',
+		cardDetailedText: [`Developed "AI Hub" platform as frontend developer`],
+	},
+	{
+		title: '08.2021 ~ 11.2021',
+		url: 'http://mondrian.ai',
+		cardTitle: 'Mondrian AI(SI)',
+		cardSubtitle: '$0.9 million in annual sales',
+		cardDetailedText: [`Developed "Data visualization Portal" for SK discovery as frontend developer`],
+	},
+	{
+		title: '04.2021 ~ Present',
+		url: 'http://www.kb-sys.co.kr/en/index',
+		cardTitle: 'BugunFNC(Woman Shopping Mall)',
+		cardSubtitle: '$19 million in annual sales',
+		cardDetailedText: [`Developed independent shopping mall "VELY VELY' and "Influencer Mall" as frontend developer`],
+	},
+];
+
+export const activityItems: TimelineItemModel[] = [
+	{
+		title: '11.2013 - 08.2015',
+		url: 'https://angelhackseoul.kr/welcome/',
+		cardTitle: `Republic of Korea Army (2230 base)`,
+		cardSubtitle: 'Discharged Republic of Korea Army as Sergeant',
+		cardDetailedText: [`Head Quarter`, `53 Aumminition Supply Point.`],
+	},
+	{
+		title: '2020',
+		url: 'https://angelhackseoul.kr/welcome/',
+		cardTitle: `AngelHack`,
+		cardSubtitle: 'HackerThon',
+		cardDetailedText: [`Participated as the development team leader of the hackathon operation team`],
+	},
+];
+
+export const educationItems: TimelineItemModel[] = [
+	{
+		title: '2011 - 2013',
+		url: 'https://ko.wikipedia.org/wiki/대성고등학교_(강원)',
+		cardTitle: `DeaSeoung HighSchool`,
+		cardSubtitle: 'Natural Science',
+		// cardDetailedText: [``],
+	},
+	{
+		title: '2017',
+		url: 'https://ko.wikipedia.org/wiki/대성고등학교_(강원)',
+		cardTitle: `Thejoeun IT Academy`,
+		cardSubtitle: 'Big data analysis and application software engineering training course ',
+		// cardDetailedText: [`Big data analysis and application software engineering training course `],
+	},
+];

2a8a588e071e69741c54df417f7c29d1e75ebd0e
LOGANLEEE <logan_lee@kakao.com>
2022-01-07 22:29:41 +0000

    add profile image
    mod 'info'
    
    add timeline comps

--- src/containers/ForMobile/index.tsx
+++ src/containers/ForMobile/index.tsx
@@ -1,14 +1,16 @@
-import { Button } from 'components/Button';
 import { ImageComp } from 'components/ImageComp';
 import { TimeLine } from 'components/TimeLine';
-import React, { memo, useCallback, useContext, useState } from 'react';
+import React, { memo } from 'react';
 import { Wrapper } from './Wrapper';
+import profile_image from 'asset/profile.jpg';
+import { activityItems, companyItems, educationItems } from './timeLineItems';
 
 export const ForMobile = memo(() => {
 	return (
 		<Wrapper>
 			<div className='intro'>
 				<ImageComp
+					imagePath={profile_image}
 					radius={15}
 					width={'50%'}
 					height={'30%'}
@@ -16,17 +18,21 @@ export const ForMobile = memo(() => {
 					// height={300}
 				/>
 				<div className='info'>
-					{/* <span className='name'>LOGAN LEE</span>
-					<span className='desc'>Full Stack Developer</span> */}
-					LOGAN LEE 🇰🇷
-					<br />
-					Fullstack Developer(4y)
-					<br />
-					<br />
+					<span>LOGAN LEE 🇰🇷</span>
+					<span>Fullstack Developer(4y)</span>
+					<a href='mailto:logan_lee@kakao.com'>logan_lee@kakao.com</a>
+					<span>Github, LinkedIn</span>
 				</div>
 			</div>
+			<br />
 			<div className='headline'>Strongly believe programming is "Creation" not "Manufacturing".</div>
-			<TimeLine />
+			<br />
+			<hr />
+			<TimeLine title='Experience' items={companyItems} isMobile />
+			<hr />
+			<TimeLine title='Activity' items={activityItems} isMobile />
+			<hr />
+			<TimeLine title='Education' items={educationItems} isMobile />
 		</Wrapper>
 	);
 });
